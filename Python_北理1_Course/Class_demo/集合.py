b0d829bb35005c49be7fe8c0909601b0599192ed
xuruizhu <xuruizhu@yeah.net>
2018-11-06 22:52:03 +0800

    modify for while4

---
+++ Python_北理1_Course/Class_demo/集合.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2018/11/6/22:41
+# @Author   : Hester Xu
+# Email     : xuruizhu@yeah.net
+# @File     : 集合.py
+# @Software : PyCharm
+
+'''
+try:
+    while True:
+        print(A.pop(),end="")
+except:
+    pass
+A = set('p123y')
+'''
+
+
+# print('p' in {'p','y',123})
+ls = ['p','p','y','y',123]
+s = set(ls)
+print(s)
+
+ls_2 = list(s)
+print(ls_2)
+
